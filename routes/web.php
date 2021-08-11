b474bf5f9d7fffc50f332fd8f43563ad1fc1a7d9
Mousa7 <mousashawa-2012@hotmail.com>
2021-08-11 12:25:30 +0300

    Update Feature

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('admin',[\App\Http\Controllers\HomeController::class,'dashboard'])->name('dashboard');
+Route::get('/',[\App\Http\Controllers\HomeController::class,'dashboard'])->name('dashboard');
 
 Auth::routes();
 
@@ -43,5 +43,5 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function (){
 
 
 });
-Route::get('/', [App\Http\Controllers\HomeController::class, 'logout'])->name('logout');
+Route::get('/logout', [App\Http\Controllers\HomeController::class, 'logout'])->name('logout');
 
